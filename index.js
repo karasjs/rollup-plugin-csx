4796d67fe00aa12744eebb801cf7ae59d5d03bcc
army8735 <army8735@qq.com>
2020-08-06 10:49:22 +0800

    feat: sourcemap

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var yurine = require('yurine');
+var MagicString = require( 'magic-string' );
 
 module.exports = function() {
   return {
     name: 'karas',
     transform: function(code) {
-      return yurine.parse(code);
+      var s = new MagicString(code);
+      var out = yurine.parse(code);
+      s.overwrite(0, code.length, out.toString());
+      return {
+        code: s.toString(),
+        map: s.generateMap({ hires: true })
+      };
     },
   };
 };
